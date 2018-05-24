166ff52d4f5c87c68e2b51a74982259badca4021
Arun Wilson <cyberinfoscripter@gmail.com>
2018-05-24 12:01:49 +0530

    Added SET_VISIBILITY_REDUCER type handler to reducer;

--- store/reducers.js
+++ store/reducers.js
@@ -1,18 +1,30 @@
+import { SET_VISIBILITY_FILTER } from "./actions.js";
+
 const visibilityFilters = require("./actions.js").VisibilityFilters;
 
 const initialState = {
-    visibilityFilter:visibilityFilters.SHOW_ALL,
-    todos:[],
-}
+  visibilityFilter: visibilityFilters.SHOW_ALL,
+  todos: []
+};
 
 function todoApp(state, action) {
-
-    if(typeof state == 'undefined'){
-
-        return initialState;
-
-    }
-
+  if (typeof state == "undefined") {
     return initialState;
-    
+  }
+
+  switch (action.type) {
+    case SET_VISIBILITY_FILTER:
+
+        return Object.assign({},state, {
+            visibilityFilter:action.filter
+        })
+
+        break;
+
+    default:
+        return state;
+
+  }
+
+  return state;
 }

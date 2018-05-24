bd6a271c16eb7dc57affb3d5d5a865524eb6fa5b
Arun Wilson <cyberinfoscripter@gmail.com>
2018-05-24 15:43:20 +0530

    Checking initial state

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
+const actionImports = require("./actions.js");
+
+const visibilityFilters = actionImports.visibilityFilters;
+const SET_VISIBILITY_FILTER = actionImports.SET_VISIBILITY_FILTER;
+const TOGGLE_TODO = actionImports.TOGGLE_TODO;
+const ADD_TODO = actionImports.ADD_TODO;
+
+
 const createStore = require('redux').createStore;
 const todoApp = require('./store/reducers.js').todoApp;
 
 const store = createStore(todoApp);
+
+/* testing App */
+console.log('Initial State : ',store.getState());
